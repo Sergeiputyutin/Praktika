f7a844c6c1a14330d2bffa6934195949553c450a
MaksimKhmelkov <khmelkov.2000maksim@gmail.com>
2025-06-26 23:31:11 +0300

    Update proektchast1.cpp
    Протестировал программу, в ходе тестирования вывел не корректную работу программы, исправил генерацию случайных чисел, добавил счетчик времени выполнения алгоритма Шейкерной сортировки, также добавил счетчик перестановок выполненных при сортировки.

--- proektchast1.cpp
+++ proektchast1.cpp
@@ -4,9 +4,12 @@
 #include <cstdlib>
 #include <ctime>
 #include <limits>
-#include <locale> // Для setlocale
+#include <locale>
+#include <chrono> // Для измерения времени
+#include <random> // Добавляем для современного генератора случайных чисел
 
 using namespace std;
+using namespace std::chrono; // Для измерения времени
 
 // Функция для записи массива в файл
 void saveToFile(const string& filename, const vector<int>& arr) {
@@ -22,19 +25,23 @@ void saveToFile(const string& filename, const vector<int>& arr) {
     cout << "Массив сохранён в файл: " << filename << endl;
 }
 
-// Шейкерная сортировка
-void cocktailSort(vector<int>& arr) {
+// Шейкерная сортировка с подсчётом операций и времени
+void cocktailSort(vector<int>& arr, int& comparisons, int& swaps) {
     bool swapped = true;
     int start = 0;
     int end = arr.size() - 1;
+    comparisons = 0;
+    swaps = 0;
 
     while (swapped) {
         swapped = false;
 
         // Проход слева направо
         for (int i = start; i < end; ++i) {
+            comparisons++;
             if (arr[i] > arr[i + 1]) {
                 swap(arr[i], arr[i + 1]);
+                swaps++;
                 swapped = true;
             }
         }
@@ -46,8 +53,10 @@ void cocktailSort(vector<int>& arr) {
 
         // Проход справа налево
         for (int i = end - 1; i >= start; --i) {
+            comparisons++;
             if (arr[i] > arr[i + 1]) {
                 swap(arr[i], arr[i + 1]);
+                swaps++;
                 swapped = true;
             }
         }
@@ -57,32 +66,40 @@ void cocktailSort(vector<int>& arr) {
 }
 
 // Генерация случайного массива в заданном диапазоне
+
 vector<int> generateRandomArray(int size, int minVal, int maxVal) {
     vector<int> arr(size);
-    srand(time(0));
+
+    // Инициализация генератора случайных чисел
+    random_device rd;
+    mt19937 gen(rd());
+    uniform_int_distribution<int> dist(minVal, maxVal);
+
     for (int i = 0; i < size; ++i) {
-        arr[i] = minVal + rand() % (maxVal - minVal + 1);
+        arr[i] = dist(gen);
     }
     return arr;
 }
 
 int main() {
-    // Устанавливаем локаль для поддержки русского языка
     setlocale(LC_ALL, "Russian");
 
     vector<int> arr;
     int choice;
+    int comparisons = 0;
+    int swaps = 0;
 
     do {
         cout << "\n=== Меню шейкерной сортировки ===\n";
         cout << "1. Ввести массив вручную\n";
         cout << "2. Сгенерировать случайный массив\n";
         cout << "3. Отсортировать массив и сохранить в файлы\n";
-        cout << "4. Вывести текущий массив\n";
+        cout << "4. Вывести заполненый массив\n";
         cout << "5. Выход\n";
         cout << "Выберите действие: ";
         cin >> choice;
-    switch (choice) {
+
+        switch (choice) {
         case 1: {
             int n, num;
             cout << "Введите количество элементов: ";
@@ -117,10 +134,24 @@ int main() {
             }
             else {
                 vector<int> sortedArr = arr;
-                cocktailSort(sortedArr);
+
+                // Начало измерения времени
+                auto start_time = high_resolution_clock::now();
+
+                cocktailSort(sortedArr, comparisons, swaps);
+
+                // Конец измерения времени
+                auto end_time = high_resolution_clock::now();
+                auto duration = duration_cast<microseconds>(end_time - start_time);
+
                 saveToFile("sorted.txt", sortedArr);
                 cout << "Исходный массив сохранён в unsorted.txt\n";
                 cout << "Отсортированный массив сохранён в sorted.txt\n";
+
+                // Вывод статистики
+                cout << "\nСтатистика сортировки:\n";
+                cout << "Время сортировки: " << duration.count() << " микросекунд\n";
+                cout << "Количество перестановок: " << swaps << endl;
             }
             break;
         }
